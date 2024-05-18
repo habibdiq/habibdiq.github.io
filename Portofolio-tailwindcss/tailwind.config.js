5ece3bbe91ba18bce6accd0d8c957503a8a23b1a
habibdiq <habibsiddiq1002@gmail.com>
2024-05-18 09:06:41 +0700

    Add files via upload

---
+++ Portofolio-tailwindcss/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center:true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary:'#14b8a6',
+        secondary:'#64748b',
+        dark:'#0f172a',
+        abu:'#cbd5e1',
+        bg:'#2D3250',
+        
+      },
+      screens:{
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
